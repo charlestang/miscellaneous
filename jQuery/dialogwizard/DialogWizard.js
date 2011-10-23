b2c1ddd28f7e59fa409c1327025d672d8afa019f
Charles Tang <charlestang@foxmail.com>
2011-10-23 22:42:05 +0800

    init

---
+++ jQuery/dialogwizard/DialogWizard.js
@@ -0,0 +1,21 @@
+/*
+ * jQuery UI Plugins: Dialog Wizard
+ * 
+ * Author: Charles <charlestang@foxmail.com>
+ *
+ * Depends:
+ *      jquery.ui.dialog.js
+ *	jquery.ui.core.js
+ *	jquery.ui.widget.js
+ *      jquery.ui.button.js
+ *	jquery.ui.draggable.js
+ *	jquery.ui.mouse.js
+ *	jquery.ui.position.js
+ *	jquery.ui.resizable.js
+ */
+(function($, undefined){ //the wrapper of the plugin
+    
+$.widget();
+    
+    
+}(jQuery)); // end the wrapper
